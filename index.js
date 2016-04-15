c422f945e21155fbf4cecb22d796f11f04a87c42
Nick Poisson <nick.poisson@gmail.com>
2016-04-14 23:28:47 -0400

    :arrow_down: :arrow_lower_right: :arrow_right: :b: :boom:

---
+++ index.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var path = require('path');
+var glob = require('glob');
+
+module.exports = function(url, parent, done) {
+  var base = path.join(path.dirname(parent),url);
+  if (glob.hasMagic(base)) {
+    glob(base,{nodir: true},function(err,files) {
+      var contents = files.map(function(cur) {
+        return fs.readFileSync(cur,'utf8');
+      });
+      done({content: contents.join('\n')});
+    });
+  } else {
+    done({file: url});
+  }
+}
